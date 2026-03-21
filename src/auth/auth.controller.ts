6ee7fda8fdef036c906dce9f962ebc72522e65b6
Israel Rodrigues Medeiros <israel.medeiros@dbccompany.com.br>
2026-03-21 15:35:52 -0300

    feat: implementa auth guard e protege rotas da api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,11 +10,19 @@ export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post('login')
-    @HttpCode(HttpStatus.OK) 
+    @HttpCode(HttpStatus.OK)
     @ApiOperation({ summary: 'Realiza o login e retorna o token JWT' })
     @ApiResponse({ status: 200, description: 'Login bem-sucedido.' })
     @ApiResponse({ status: 401, description: 'Credenciais inválidas.' })
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
+    }
+
+    @UseGuards(AuthGuard)
+    @ApiBearerAuth()
+    @Get('profile')
+    @ApiOperation({ summary: 'Retorna os dados do utilizador autenticado' })
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
